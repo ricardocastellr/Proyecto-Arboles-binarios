42cbe3d6e807ae554b5d0f705aa9503719cea8ca
ricardocastellr <rcastell@ucol.mx>
2022-11-26 19:49:48 -0600

    Creacion de clases

---
+++ main.js
@@ -0,0 +1,12 @@
+class Nodo{
+    constructor(simbolo){
+        this.simbolo = simbolo;
+        this.hijoDerecha = null;
+        this.hijoIzquierda = null;
+    }
+}
+class Arbol{
+    constructor(){
+        this.raiz = null;
+    }
+}
