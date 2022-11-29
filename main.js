0b40a397113c217eac590fe4a062859e388df3f9
ricardocastellr <rcastell@ucol.mx>
2022-11-28 20:18:29 -0600

    Probable final, sino ocurre fallos

--- main.js
+++ main.js
@@ -8,9 +8,56 @@ class Nodo{
     }
 }
 class ArbolBinario{
-    constructor(){
+    constructor(expresion){
         this.raiz = null;
         this.resultado = "";
+        this.expresion = expresion;
+        this.expresionVector = new Array(); 
+        for (let i = 0; i < expresion.length; i++) { 
+            this.expresionVector.push(new Nodo(expresion[i]))     
+        }
+    }
+
+    crearArbol(){ //Creación del arbol
+        let aux = this.expresionVector[0];
+        for(let i=0; aux;){
+            //Condición de jerarquía con potencia.
+            if((aux != null) && ((aux.dato == "^"))){
+                aux.hijoIzquierda = this.expresionVector[i - 1];
+                aux.hijoDerecha = this.expresionVector[i + 1];
+                this.expresionVector.splice(i + 1, 1);
+                this.expresionVector.splice(i - 1, 1);
+            }
+            i++;
+            aux = this.expresionVector[i];
+        }
+        aux = this.expresionVector[0];
+        for(let i=0; aux;){
+            //Condición de jerarquía con divisón y multiplicación.
+            if((aux.dato == "/") || (aux.dato == "*")){
+                aux.hijoIzquierda = this.expresionVector[i - 1];
+                aux.hijoDerecha = this.expresionVector[i + 1];
+                this.expresionVector.splice(i + 1, 1);
+                this.expresionVector.splice(i - 1, 1);
+                i--;
+            }
+            i++;
+            aux = this.expresionVector[i];
+        }
+        aux = this.expresionVector[0];
+        for(let i=0; aux;){
+            // Condición de jerarquía con resta y suma.
+            if((aux.dato == "-") || (aux.dato == "+")){
+                aux.hijoIzquierda = this.expresionVector[i - 1];
+                aux.hijoDerecha = this.expresionVector[i + 1];
+                this.expresionVector.splice(i + 1, 1);
+                this.expresionVector.splice(i - 1, 1); 
+                i--;  
+            }
+            i++;
+            aux = this.expresionVector[i];
+        }
+        this.raiz = this.expresionVector[0];
     }
 
     preOrder(){
@@ -45,84 +92,6 @@ class ArbolBinario{
         if(nodoX.hijoDerecha != null)
             this.postOrderRecursiva(nodoX.hijoDerecha);
         this.resultado += `${nodoX.dato}`;
-    }
-}
-
-class Lista{
-    constructor(){
-        this.first = null;
-        this.last = null;
-    }
-
-    agregar(nuevo){
-        if(this.first == null){
-            this.first = nuevo;
-            this.last = nuevo;
-        }
-        else{
-            nuevo.before = this.last;
-            this.last.next = nuevo;
-            this.last = nuevo
-        }
-    }
-
-    crearArbol(){ //Creación del arbol
-        let aux = this.first;
-        while(aux){
-            //Condición de jerarquía con potencia.
-            if((aux != null) && ((aux.dato == "^"))){
-                aux.hijoIzquierda = aux.before;
-                aux.hijoDerecha = aux.next;
-                aux.next = aux.next.next;
-                aux.before = aux.before.before;
-                if(aux.before != null)
-                    aux.before.next = aux;
-                if(aux.next != null)
-                    aux.next.before = aux;
-            }
-            aux = aux.next;
-        }
-        aux = this.first;
-        while(aux){
-            //Condición de jerarquía con divisón y multiplicación.
-            if((aux.dato == "/") || (aux.dato == "*")){
-                aux.hijoIzquierda = aux.before;
-                aux.hijoDerecha = aux.next;
-                aux.next = aux.next.next;
-                aux.before = aux.before.before;
-                if(aux.before != null)
-                    aux.before.next = aux;
-                if(aux.next != null)
-                    aux.next.before = aux;
-            }
-            aux = aux.next;
-        }
-        aux = this.first;
-        while(aux){
-            // Condición de jerarquía con resta y suma.
-            if((aux.dato == "-") || (aux.dato == "+")){
-                aux.hijoIzquierda = aux.before;
-                aux.hijoDerecha = aux.next;
-                aux.next = aux.next.next;
-                aux.before = aux.before.before;
-                if(aux.before != null)
-                    aux.before.next = aux;
-                if(aux.next != null)
-                    aux.next.before = aux;    
-            }
-            if(aux.next == null)
-                return aux;
-            aux = aux.next;
-        }
-    }
-}
-
-class Analisis{
-    convertirExpresion(expresion, lista){
-        for (let i=0; i<expresion.length ;i++) {
-            let nodo = new Nodo(Array.from(expresion)[i]);
-            lista.agregar(nodo);
-        }
     }
 
     acomodo(expresionOrder){
@@ -194,7 +163,7 @@ class Analisis{
                     resultado.push(parseInt(aux));
             }
         }
-
+    
         return resultado;
     }
 }
@@ -210,11 +179,9 @@ expresion = "2*3*4/8-4*3/6-9/3*6/2^2";
 //expresion = "3*3^3";
 
 //NO MOVER, NECESARIO PARA FUNCIONAR
-let arbolbinario = new ArbolBinario(); // Creamos el arbol binario.
-let lista = new Lista(); // Creamos la lista.
-let analisis = new Analisis(); // Creamos el analisis.
-analisis.convertirExpresion(expresion,lista); // Convertimos la expresión a una lista doble enlazada.
-arbolbinario.raiz = lista.crearArbol(); // La raíz del arbol binario es igual a crear arbol, empezamos la creación del arbol desde la raíz.
+// Creamos la clase ArbolBinario y convertimos la expresión a array.
+let arbolbinario = new ArbolBinario(expresion);
+arbolbinario.crearArbol(); // Creamos el arbol binario.
 let preOrder = arbolbinario.preOrder(); // Creamos el preOrder y lo guardamos en una variable.
 let postOrder = arbolbinario.postOrder(); // Creamos el postOrder y lo guardamos en una variable.
 console.log(`Expresión: ${expresion}`); // Mostramos la expresión ingresada.
@@ -223,6 +190,6 @@ console.log(`preOrder: ${preOrder}`); // Imprimimos el preOrder.
 console.log(`postOrder: ${postOrder}`); // Imprimimos el postOrder.
 console.log("--------------------------------------------------------------------------");
 // Resultado de la expresión en base al preOrder.
-console.log(`Resultado en preOrder: ${analisis.acomodo(preOrder)}`)
+console.log(`Resultado en preOrder: ${arbolbinario.acomodo(preOrder)}`)
 // Resultado de la expresión en base al postOrder.
-console.log(`Resultado en postOrder: ${analisis.acomodo(postOrder)}`)
+console.log(`Resultado en postOrder: ${arbolbinario.acomodo(postOrder)}`)
